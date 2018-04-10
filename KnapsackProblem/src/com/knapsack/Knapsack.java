33b6e7c3308f9395f439a084b06c826705c23507
Abhinn Ankit <ankit.a@husky.neu.edu>
2018-04-10 02:13:03 -0400

    Input method added

---
+++ KnapsackProblem/src/com/knapsack/Knapsack.java
@@ -0,0 +1,16 @@
+package com.knapsack;
+
+import java.util.ArrayList;
+
+public class Knapsack {
+
+    private int numberOfItems;
+    private int knapsackCapacity;
+    private ArrayList<Double> valueOfItems;
+    private ArrayList<Double> weightOfItems;
+
+    public Knapsack() {
+
+    }
+
+}
