c92a7b6d162597bbf36401f21a0ab8bbc71bdee1
Abhinn Ankit <ankit.a@husky.neu.edu>
2018-04-11 01:17:00 -0400

    Added data members in GA class

--- KnapsackProblem/src/com/knapsack/GeneticAlgorithm.java
+++ KnapsackProblem/src/com/knapsack/GeneticAlgorithm.java
@@ -2,6 +2,16 @@ package com.knapsack;
 
 public class GeneticAlgorithm {
 
+    private Knapsack knapsack;
+    private int populationSize;
+    private int maxGenerations;
+    private double mutationProbability;
 
+    public GeneticAlgorithm(Knapsack knapsack, int populationSize, int maxGenerations, double mutationProbability) {
+        this.knapsack = knapsack;
+        this.populationSize = populationSize;
+        this.maxGenerations = maxGenerations;
+        this.mutationProbability = mutationProbability;
+    }
 
 }
--- KnapsackProblem/src/com/knapsack/Main.java
+++ KnapsackProblem/src/com/knapsack/Main.java
@@ -13,12 +13,13 @@ public class Main {
         System.out.println("Enter the maximum number of generations: ");
         int maxGenerations = (int) knapsack.inputNumber();
 
-        System.out.println("Enter the crossover probability: ");
-        double crossoverProbability = (double) knapsack.inputNumber();
+//        System.out.println("Enter the crossover probability: ");
+//        double crossoverProbability = (double) knapsack.inputNumber();
 
         System.out.println("Enter the mutation probability: ");
         double mutationProbability = (double) knapsack.inputNumber();
 
+        GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm(knapsack, populationSize, maxGenerations, mutationProbability);
 
     }
 
