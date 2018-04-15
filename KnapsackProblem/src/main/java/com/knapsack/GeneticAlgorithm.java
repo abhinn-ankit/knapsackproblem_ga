5554926c6e1348657ee6650cce9adb524e221471
Abhinn Ankit <ankit.a@husky.neu.edu>
2018-04-15 00:52:46 -0400

    Cull method added

--- KnapsackProblem/src/main/java/com/knapsack/GeneticAlgorithm.java
+++ KnapsackProblem/src/main/java/com/knapsack/GeneticAlgorithm.java
@@ -8,7 +8,7 @@ public class GeneticAlgorithm {
     Knapsack knapsack;
     int generationNo;
     Population population;
-    ArrayList<Individual> fittestIndividualOfGeneration;
+    ArrayList<Individual> fittestOfGenerations;
     ArrayList<Individual> children;
 
     //Input variables
@@ -24,7 +24,7 @@ public class GeneticAlgorithm {
         this.mutationProbability = mutationProbability;
         this.crossoverProbability = crossoverProbability;
         this.generationNo = 0;
-        this.fittestIndividualOfGeneration = new ArrayList<>();
+        this.fittestOfGenerations = new ArrayList<>();
         this.children = new ArrayList<>();
         this.population = new Population(this.sizeOfPopulation, this.knapsack);
     }
@@ -34,11 +34,11 @@ public class GeneticAlgorithm {
         this.population.seed();
         while (this.generationNo <= this.maxGenerations) {
             if (this.generationNo > 10)
-                if (this.fittestIndividualOfGeneration.get(generationNo - 1).equals(this.fittestIndividualOfGeneration.get(generationNo - 2))
-                        && this.fittestIndividualOfGeneration.get(generationNo - 1).equals(this.fittestIndividualOfGeneration.get(generationNo - 3))
-                        && this.fittestIndividualOfGeneration.get(generationNo - 1).equals(this.fittestIndividualOfGeneration.get(generationNo - 4))
-                        && this.fittestIndividualOfGeneration.get(generationNo - 1).equals(this.fittestIndividualOfGeneration.get(generationNo - 5))
-                        && this.fittestIndividualOfGeneration.get(generationNo - 1).equals(this.fittestIndividualOfGeneration.get(generationNo - 6))) {
+                if (this.fittestOfGenerations.get(generationNo - 1).equals(this.fittestOfGenerations.get(generationNo - 2))
+                        && this.fittestOfGenerations.get(generationNo - 1).equals(this.fittestOfGenerations.get(generationNo - 3))
+                        && this.fittestOfGenerations.get(generationNo - 1).equals(this.fittestOfGenerations.get(generationNo - 4))
+                        && this.fittestOfGenerations.get(generationNo - 1).equals(this.fittestOfGenerations.get(generationNo - 5))
+                        && this.fittestOfGenerations.get(generationNo - 1).equals(this.fittestOfGenerations.get(generationNo - 6))) {
                     System.out.println("\nStop criterion met");
                 }
             createGenerations();
@@ -54,36 +54,70 @@ public class GeneticAlgorithm {
         if (this.generationNo > 0)
             cull();
 
-        for (int j = 0; j < this.sizeOfPopulation / 2; j++) {
-
+        for (int i = 0; i < this.sizeOfPopulation / 2; i++) {
+            Individual individual1 = selectIndividual(generationTotalFitness);
+            Individual individual2 = selectIndividual(generationTotalFitness);
+            crossover(individual1, individual2);
         }
+
+        if (this.sizeOfPopulation % 2 == 1) {
+            Individual individual1 = selectIndividual(generationTotalFitness);
+            Individual individual2 = selectIndividual(generationTotalFitness);
+            crossover(individual1, individual2);
+        }
+
+        this.population.calcIndividualFitness(this.children);
+        Population.sort(this.children);
+
+        for (int i = 0; i < this.sizeOfPopulation; i++) {
+            this.population.individuals.set(i, this.children.get(i));
+        }
+
+        // Output population
+        System.out.println("\nGeneration " + (this.generationNo + 1) + ":");
+        if ((this.generationNo + 1) < 10) {
+            System.out.println("=============");
+        }
+        if ((this.generationNo + 1) >= 10) {
+            System.out.println("==============");
+        }
+        if ((this.generationNo + 1) >= 100) {
+            System.out.println("===============");
+        }
+        System.out.println("Population:");
+        for (int l = 0; l < this.sizeOfPopulation; l++) {
+            System.out.println((l + 1) + " - " + this.population.individuals.get(l));
+        }
+
+        this.children.clear();
+
+        this.population.sort();
+
+        this.fittestOfGenerations.add(this.population.individuals.get(0));
+        // Output best fitness of generation
+        System.out.println("Fitness score of best solution of generation " + (this.generationNo + 1) +
+                ": " + this.fittestOfGenerations.get(this.generationNo));
+
     }
 
     public void cull() {
+        double rand = Math.random();
+        int keep = 0;
 
-    }
-
-    private void crossoverGenes(Individual individual1, Individual individual2) {
-
-        double crossover = Math.random();
-
-        if (crossover <= crossoverProbability) {
-            Random random = new Random();
-            int swapIndex = random.nextInt(knapsack.knapsackCapacity) + 1;
-            String tempGene1 = individual1.gene.substring(0, swapIndex) + individual2.gene.substring(swapIndex);
-            String tempGene2 = individual2.gene.substring(0, swapIndex) + individual1.gene.substring(swapIndex);
-            this.children.add(new Individual(tempGene1));
-            this.children.add(new Individual(tempGene2));
+        if (rand * sizeOfPopulation > 2) {
+            keep = (int) Math.floor(rand * sizeOfPopulation);
         } else {
-            this.children.add(individual1);
-            this.children.add(individual2);
+            keep = 2;
         }
+        // sort the population
+        population.sort();
 
-        // Check if mutation is to be performed
-//        mutateGene();
+        for (int i = sizeOfPopulation-keep+1; i < sizeOfPopulation; i++) {
+            population.individuals.remove(i);
+        }
     }
 
-    private Individual selectGene(double generationTotalFitness) {
+    private Individual selectIndividual(double generationTotalFitness) {
 
         // Generate random number between 0 and total_fitness_of_generation
         double rand = Math.random() * generationTotalFitness;
@@ -96,8 +130,69 @@ public class GeneticAlgorithm {
             rand = rand - individual.fitnessScore;
         }
 
-        // Not reachable; default return value
         return population.individuals.get(0);
+    }
+
+    private void crossover(Individual individual1, Individual individual2) {
+
+        double crossover = Math.random();
+
+        if (crossover <= crossoverProbability) {
+            Random random = new Random();
+            int swapIndex = random.nextInt(knapsack.numberOfItems) + 1;
+            String tempGene1 = individual1.gene.substring(0, swapIndex) + individual2.gene.substring(swapIndex);
+            String tempGene2 = individual2.gene.substring(0, swapIndex) + individual1.gene.substring(swapIndex);
+            this.children.add(new Individual(tempGene1));
+            this.children.add(new Individual(tempGene2));
+        } else {
+            this.children.add(individual1);
+            this.children.add(individual2);
+        }
+
+        // Perform mutation if required
+        mutateGene();
+    }
+
+    private void mutateGene() {
+
+        // Random mutation
+        double rand_mutation = Math.random();
+        if (rand_mutation <= mutationProbability) {
+
+            Individual mutatedIndividual;
+            Random random = new Random();
+
+            // Mutation for which child
+            boolean whichChild = random.nextBoolean();
+
+            // Mutate gene
+            if (whichChild) {
+                mutatedIndividual = this.children.get(this.children.size() - 1);
+                int index = random.nextInt(knapsack.numberOfItems);
+                StringBuilder mutatedGene = new StringBuilder(mutatedIndividual.gene);
+                if (mutatedIndividual.gene.charAt(index) == '1') {
+                    mutatedGene.setCharAt(index, '0');
+                    this.children.set(this.children.size() - 1, new Individual(mutatedGene.toString()));
+                }
+                if (mutatedIndividual.gene.charAt(index) == '0') {
+                    mutatedGene.setCharAt(index, '1');
+                    this.children.set(this.children.size() - 1, new Individual(mutatedGene.toString()));
+                }
+            }
+            if (!whichChild) {
+                mutatedIndividual = this.children.get(this.children.size() - 2);
+                int index = random.nextInt(knapsack.numberOfItems);
+                StringBuilder mutatedGene = new StringBuilder(mutatedIndividual.gene);
+                if (mutatedIndividual.gene.charAt(index) == '1') {
+                    mutatedGene.setCharAt(index, '0');
+                    this.children.set(this.children.size() - 1, new Individual(mutatedGene.toString()));
+                }
+                if (mutatedIndividual.gene.charAt(index) == '0') {
+                    mutatedGene.setCharAt(index, '1');
+                    this.children.set(this.children.size() - 1, new Individual(mutatedGene.toString()));
+                }
+            }
+        }
     }
 
 }
--- KnapsackProblem/src/main/java/com/knapsack/Population.java
+++ KnapsackProblem/src/main/java/com/knapsack/Population.java
@@ -5,32 +5,34 @@ import java.util.ArrayList;
 public class Population {
 
 	int sizeOfPopulation;
-    int numberOfItems;
     double generationTotalFitness;
     Knapsack knapsack;
     ArrayList<Individual> individuals;
 	
 	public Population(int sizeOfPopulation, Knapsack knapsack) {
 		this.sizeOfPopulation = sizeOfPopulation;
-        this.numberOfItems = knapsack.numberOfItems;
         this.knapsack = knapsack;
         this.individuals = new ArrayList<>();
 	}
 
-    public ArrayList<Individual> sort(){
-        ArrayList<Individual> sortedList = new ArrayList<>();
-        for (Individual in: individuals) {
-            in.calculateFitness(this.knapsack);
-            sortedList.add(in);
-        }
-        sort(sortedList);
-        return sortedList;
+    public void sort(){
+        ArrayList<Individual> temp = calcIndividualFitness(this.individuals);
+        this.individuals.clear();
+        this.individuals = temp;
+        sort(this.individuals);
     }
 
-    private static void sort(ArrayList<Individual> list) {
+    public ArrayList<Individual> calcIndividualFitness(ArrayList<Individual> individualArrayList) {
+        for (Individual in: individualArrayList) {
+            in.calculateFitness(this.knapsack);
+        }
+        return individualArrayList;
+    }
+
+    public static void sort(ArrayList<Individual> list) {
         int n = list.size();
         for (int i = 1; i < n; i++)
-            for (int j = i; j > 0 && list.get(j).compareTo(list.get(j-1)) > 0; j--) {
+            for (int j = i; j > 0 && list.get(j).compareTo(list.get(j-1)) >= 0; j--) {
                 exch(list, j, j-1);
             }
     }
@@ -48,8 +50,8 @@ public class Population {
     }
 	
     private Individual createGene() {
-        StringBuilder gene = new StringBuilder(numberOfItems);
-        for(int i = 0; i < numberOfItems; i++) {
+        StringBuilder gene = new StringBuilder(knapsack.numberOfItems);
+        for(int i = 0; i < knapsack.numberOfItems; i++) {
             double random = Math.random();
             char ch = ( random > 0.5) ? '1' : '0';
             gene.append(ch);
