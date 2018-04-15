e3dd63ee85645fd2e0f8a3b12ae7c287da7cb10c
joshi-aditya <joshi.adit@husky.neu.edu>
2018-04-15 18:22:05 -0400

    changed logs

--- KnapsackProblem/src/main/java/com/knapsack/Main.java
+++ KnapsackProblem/src/main/java/com/knapsack/Main.java
@@ -16,18 +16,18 @@ public class Main {
     	
     	BasicConfigurator.configure();
 
-        logger.info("Enter the Maximum population size: ");
+        logger.info("\nEnter the Maximum population size: ");
         int maxPopulationSize = (int) inputNumber();
 
-        logger.info("Enter the maximum number of generations: ");
+        logger.info("\nEnter the maximum number of generations: ");
         int maxGenerations = (int) inputNumber();
 
-        logger.info("Enter the mutation probability: ");
+        logger.info("\nEnter the mutation probability: ");
         double mutationProbability = (double) inputNumber();
 
         Knapsack knapsack = populateKnapsack();
         
-        logger.info("Starting genetic algorithm");
+        logger.info("\nStarting genetic algorithm");
         GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm(knapsack, maxPopulationSize, maxGenerations, mutationProbability);
         geneticAlgorithm.start();
 
@@ -36,16 +36,16 @@ public class Main {
     public static Knapsack populateKnapsack() {
         Scanner c = new Scanner(System.in);
 
-        logger.info("Enter the knapsack capacity: ");
+        logger.info("\nEnter the knapsack capacity: ");
         int knapsackCapacity = (int) inputNumber();
 
-        logger.info("Enter the number of items: ");
+        logger.info("\nEnter the number of items: ");
         int numberOfItems = (int) inputNumber();
 
-        logger.info("Enter the max-weight for items: ");
+        logger.info("\nEnter the max-weight for items: ");
         int maxWeight = (int) inputNumber();
 
-        logger.info("Enter the max-values for items: ");
+        logger.info("\nEnter the max-values for items: ");
         int maxValue = (int) inputNumber();
 
         Random randomValue = new Random(10);
@@ -57,7 +57,7 @@ public class Main {
         for (int i = 0; i < numberOfItems; i++) {
             itemValues.add(randomValue.nextInt(maxValue) + 1);
             itemWeights.add(randomWeight.nextInt(maxWeight) + 1);
-            logger.info("Values"+(i+1)+": "+itemValues.get(i));
+            logger.info("\nValues"+(i+1)+": "+itemValues.get(i));
             logger.info("  ----  Weights"+(i+1)+": "+itemWeights.get(i));
         }
 
@@ -98,10 +98,10 @@ public class Main {
             } else if (isDouble(input)) {
                 return Double.parseDouble(input);
             } else {
-                logger.info("Not a number. Would you like to try again?(Y/n)");
+                logger.info("\nNot a number. Would you like to try again?(Y/n)");
                 ch = c.next().charAt(0);
                 String temp = c.nextLine();
-                logger.info("Enter the number");
+                logger.info("\nEnter the number");
             }
             if (ch == 'n' || ch == 'N')
                 System.exit(1);
